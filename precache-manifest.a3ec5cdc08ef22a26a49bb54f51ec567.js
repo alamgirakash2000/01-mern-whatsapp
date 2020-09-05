773b13002fd3047367383828b4b3d2c424f5a192
Md. Alamgir Hossain <alamgirakash2000@gmail.com>
2020-09-06 01:34:13 +0600

    Updates

---
+++ precache-manifest.a3ec5cdc08ef22a26a49bb54f51ec567.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f50344844c40f7c6ffc583a73cb505f",
+    "url": "/01-mern-whatsapp/index.html"
+  },
+  {
+    "revision": "95a81beaa25f67e57bc2",
+    "url": "/01-mern-whatsapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/01-mern-whatsapp/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/01-mern-whatsapp/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a81beaa25f67e57bc2",
+    "url": "/01-mern-whatsapp/static/js/main.b03d549a.chunk.js"
+  },
+  {
+    "revision": "084a7a7848a67ff07e88",
+    "url": "/01-mern-whatsapp/static/js/runtime-main.a56e8913.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/01-mern-whatsapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
